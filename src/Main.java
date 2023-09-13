34c3a00a3e75efc8d6b3f3ea13aeddc8ef7b16cd
Ryan Truong <ryanctruong03@gmail.com>
2023-09-12 22:51:25 -0500

    binary search

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main cls = new Main();
+
+        int[] nums = {1,2,3,4};
+        int a = cls.findIndex(nums, 3);
+
+        System.out.println(a);
     }
+
+    // leet code methods
+    public int findIndex(int[] nums, int n ){
+        int i = 0, j = nums.length-1;
+        while(i <= j){
+            int mid = (j + i) / 2;
+            if(nums[i] > n){
+                j = mid-1;
+            } else if(nums[mid] < n) {
+                i = mid+1;
+            } else{
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+
+
 }
